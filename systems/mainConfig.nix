16db2c3394b53a793dbc7381cab58dfe0963fc24
VoidNoi <voidnoi@proton.me>
2025-05-30 13:50:50 +0200

    Fix boot issues

--- systems/mainConfig.nix
+++ systems/mainConfig.nix
@@ -76,7 +76,7 @@
   nixpkgs.config.allowUnfreePredicate = (pkg: true);
 
   systemd = {
-    services.syncthing-init.after = [ "graphical.target" ];
+    services.syncthing-init.after = [ "graphical-session.target" ];
     targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
     services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
   };
